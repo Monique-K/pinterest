adc9030d5b4b46a89b4dd3a407e95ff56eb4d075
Monique-K <mkhoury87@hotmail.com>
2019-02-05 09:58:19 -0500

    setting up firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Nav from './nav/nav';
+import Login from './login/login';
 import './App.scss';
 
 class App extends Component {
@@ -8,17 +9,39 @@ class App extends Component {
     this.state = {
       username: "",
       loggedIn: false,
+      modalIsActive: false,
     }
   }
 
+  handleLoginClick = () => {
+    console.log("click")
+    this.setState({modalIsActive: true})
+  }
 
   render() {
     return (
       <div className="App">
         <Nav username={this.state.username} loggedIn={this.state.loggedIn}/>
+        <Login active={this.state.modalIsActive} action={() => this.handleLoginClick()} />
       </div>
     );
   }
 }
 
 export default App;
+
+
+/*
+To Do: 
+
+- set state of app when modal opens or closes
+- register with github
+- render pic divs
+- cant close modal if app says its false
+
+- set up backend db
+
+
+
+
+*/
